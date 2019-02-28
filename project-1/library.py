b7d0c1be119fd927111218db9af55c120f9a4737
Daniel Constantine <danielkevinconstantine@gmail.com>
2019-02-27 22:29:38 -0500

    improving library, add answer to question one, two, three

--- project-1/library.py
+++ project-1/library.py
@@ -1,13 +1,17 @@
 def writeToFile(url, container):
-    bt = open(url, "w")
+    bt = open(url, 'w')
     for string in container:
         bt.write(string)
     bt.close()
+
+# padding sentences
+
 
 def padSentence():
     paddingSentence('brown-train')  # padding the brown-train file
     paddingSentence('learner-test')  # padding the learner-test file
     paddingSentence('brown-test')  # padding the brown-test file
+
 
 def paddingSentence(url):
     # constant to append and prepend
@@ -22,15 +26,17 @@ def paddingSentence(url):
     openRead = openFile.readlines()
 
     for currentLine in openRead:
-        # print(currentLine)
         currentLine = currentLine.lower()  # lowecase everything
         lastIndex = len(currentLine) - 1
-        currentLine = currentLine[:lastIndex] + " " + \
-            append + " " + currentLine[lastIndex:]
-        container.append(prepend + " " + currentLine)
+        currentLine = currentLine[:lastIndex] + ' ' + \
+            append + ' ' + currentLine[lastIndex:]
+        container.append(prepend + ' ' + currentLine)
     openFile.close()
-    
-    writeToFile(url+"-after.txt", container)
+
+    writeToFile(url+'-after.txt', container)
+
+# create dictionary
+
 
 def createDictionaryUnigram():
     d = dict()  # my dictionary
@@ -39,17 +45,18 @@ def createDictionaryUnigram():
     openRead = openFile.readlines()
 
     for currentLine in openRead:
-        for oneString in currentLine.split(" "):
+        for oneString in currentLine.split(' '):
             if oneString in d:
                 d[oneString] += 1
             else:
                 d[oneString] = 1
 
-    f = open("dictUnigram.txt", "w")
+    f = open('dictionary-unigram-before-unk-dataset.txt', 'w')
     f.write(str(d))
     f.close
 
     return d
+
 
 def createDictionaryBigram():
     d = dict()  # my dictionary
@@ -58,20 +65,35 @@ def createDictionaryBigram():
     openRead = openFile.readlines()
 
     for currentLine in openRead:
-        split = currentLine.split(" ")
+        split = currentLine.split(' ')
 
         for index in range(len(split) - 1):
-            combinedString = split[index] + " " + split[index+1]
+            combinedString = split[index] + ',' + split[index+1]
             if combinedString in d:
                 d[combinedString] += 1
             else:
                 d[combinedString] = 1
 
-    f = open("dictBigram.txt", "w")
+    f = open('dictionary-bigram-before-unk-dataset.txt', 'w')
     f.write(str(d))
     f.close
 
     return d
+
+# replacing the word with <unk>
+
+
+def replaceOccuring():
+    d = createDictionaryUnigram()  # my dictionary
+    print('--Replacing the word occured once in brown-train file with unk--')
+    replaceOccuringOnce(d, 'brown-train-after')
+
+    print('--Replacing the word not appeared in brown-test file with unk--')
+    replaceNotOccuring(d, 'brown-test-after')
+
+    print('--Replacing the word not appeared in brown-test file with unk--')
+    replaceNotOccuring(d, 'learner-test-after')
+
 
 def replaceOccuringOnce(d, url):
     # REPLACE ALL WORD OCCURING IN TRAINING DATA ONCE WITH THE TOKEN UNK
@@ -84,17 +106,18 @@ def replaceOccuringOnce(d, url):
     # REPLACE THE WORD WITH UNKNOWN
     for currentLine in openRead:
         # print(currentLine)
-        txt = ""
+        txt = ''
         currentLine = currentLine.lower()  # lowecase everything
-        for oneString in currentLine.split(" "):
+        for oneString in currentLine.split(' '):
             if d.get(oneString) == 1:
-                txt = txt + " <unk>"
+                txt = txt + ' <unk>'
             else:
-                txt = txt + " " + oneString
+                txt = txt + ' ' + oneString
         container.append(txt)
     openFile.close()
 
-    writeToFile(url+"-replaced-unk.txt", container)
+    writeToFile(url+'-replaced-unk.txt', container)
+
 
 def replaceNotOccuring(d, url):
     # REPLACE ALL WORD OCCURING IN TRAINING DATA ONCE WITH THE TOKEN UNK
@@ -107,26 +130,149 @@ def replaceNotOccuring(d, url):
     # REPLACE THE WORD WITH UNKNOWN
     for currentLine in openRead:
         # print(currentLine)
-        txt = ""
+        txt = ''
         currentLine = currentLine.lower()  # lowecase everything
-        for key in currentLine.split(" "):
+        for key in currentLine.split(' '):
             if key not in d.keys():
-                txt = txt + " <unk>"
+                txt = txt + ' <unk>'
             else:
-                # print("appear " + key)
-                txt = txt + " " + key
+                txt = txt + ' ' + key
         container.append(txt)
     openFile.close()
 
-    writeToFile(url+"-replaced-unk.txt", container)
+    writeToFile(url + '-replaced-unk.txt', container)
 
-def replaceOccuring():
-    d = createDictionaryUnigram()  # my dictionary
-    print('--Replacing the word occured once in brown-train file with unk--')
-    replaceOccuringOnce(d, 'brown-train-after')
 
-    print('--Replacing the word not appeared in brown-test file with unk--')
-    replaceNotOccuring(d, 'brown-test-after')
+def questionOne():
+    mySet = set()  # my set
+    # print(len(mySet))
+    openFile = open('brown-train-after-replaced-unk.txt', 'r')
+    openRead = openFile.readlines()
 
-    print('--Replacing the word not appeared in brown-test file with unk--')
-    replaceNotOccuring(d, 'learner-test-after')
+    for currentLine in openRead:
+        for oneString in currentLine.split(' '):
+            if oneString not in mySet:
+                mySet.add(oneString)
+
+    print('Question 1:')
+    print(len(mySet))
+
+
+def questionTwo():
+    count1 = 0
+    count2 = 0
+
+    openFile = open('brown-train-after-replaced-unk.txt', 'r')
+    openRead = openFile.readlines()
+
+    for currentLine in openRead:
+        for oneString in currentLine.split(' '):
+            # print(oneString)
+            count1 = count1 + 1
+
+    d = dict()
+    openFile = open('brown-train.txt', 'r')
+    openRead = openFile.readlines()
+
+    for currentLine in openRead:
+        for oneString in currentLine.split(' '):
+            if oneString in d:
+                d[oneString] += 1
+            else:
+                d[oneString] = 1
+
+    for value in d.values():
+        count2 = count2 + value
+
+    print('Question 2:')
+    print(count1)
+    print(count2)
+
+
+def questionThree():
+    d = createDictionaryUnigram()
+
+    # ** BROWN-TEST ** #
+    countTokenAppearsBrownTest = 0
+    countTypesAppearBrownTest = 0
+    sizeBrownTest = 0
+
+    # CREATING SET FOR TYPES (UNIQUE )
+    brownSet = set()  # my set
+    openFile = open('brown-test-after.txt', 'r')
+    openRead = openFile.readlines()
+
+    for currentLine in openRead:
+        for oneString in currentLine.split(' '):
+            if oneString not in brownSet:
+                brownSet.add(oneString)
+
+    openFile = open('brown-test-after' + '.txt', 'r')
+    openRead = openFile.readlines()
+    # word types (unique)
+    for currentLine in openRead:
+        sizeBrownTest = sizeBrownTest + \
+            len(currentLine.split(' '))
+        for key in currentLine.split(' '):
+            if key not in d.keys():
+                countTokenAppearsBrownTest = countTokenAppearsBrownTest + 1
+
+    for item in brownSet:
+        if item not in d:
+            countTypesAppearBrownTest = countTypesAppearBrownTest + 1
+
+    openFile.close()
+
+    # ** LEARNER-TEST ** #
+    countTypesAppearLearnerTest = 0
+    countTokenAppearsLearnerTest = 0
+    sizeLearnerTest = 0
+
+    # CREATING SET FOR TYPES (UNIQUE )
+    learnerSet = set()  # my set
+    openFile = open('learner-test-after.txt', 'r')
+    openRead = openFile.readlines()
+
+    for currentLine in openRead:
+        for oneString in currentLine.split(' '):
+            if oneString not in learnerSet:
+                learnerSet.add(oneString)
+
+    openFile = open('learner-test-after' + '.txt', 'r')
+    openRead = openFile.readlines()
+
+    # word types (unique)
+    for currentLine in openRead:
+        sizeLearnerTest = sizeLearnerTest + \
+            len(currentLine.split(' '))
+        for key in currentLine.split(' '):
+            if key not in d.keys():
+                countTokenAppearsLearnerTest = countTokenAppearsLearnerTest + 1
+
+    for item in learnerSet:
+        if item not in d:
+            countTypesAppearLearnerTest = countTypesAppearLearnerTest + 1
+
+    openFile.close()
+
+    print(countTokenAppearsBrownTest, ' count token appears brown test')
+    print(countTypesAppearBrownTest, ' count types appears brown test')
+    print(sizeBrownTest, ' size of brown test')
+
+    print(countTypesAppearLearnerTest, ' count type appears learner test')
+    print(countTokenAppearsLearnerTest, ' count token appears learner test')
+    print(sizeLearnerTest, ' size of learner test')
+
+
+def createSet():
+     # CREATING SET FOR TYPES (UNIQUE )
+    thisSet = set()  # my set
+    openFile = open('brown-test-after.txt', 'r')
+    openRead = openFile.readlines()
+
+    for currentLine in openRead:
+        for oneString in currentLine.split(' '):
+            if oneString not in thisSet:
+                thisSet.add(oneString)
+
+    return thisSet
--- project-1/main.py
+++ project-1/main.py
@@ -1,8 +1,13 @@
 import library
 
-print('--Start padding <unk> and </unk> and lowercasing the files respectively--')
-library.padSentence()
-print('--End of padding <unk> and </unk> and lowercasing the files respectively--')
+# print('--Start padding <unk> and </unk> and lowercasing the files respectively--')
+# library.padSentence()
+# print('--End of padding <unk> and </unk> and lowercasing the files respectively--')
 
-library.replaceOccuring()
-library.createDictionaryBigram()
+# library.replaceOccuring()
+
+# library.questionOne()
+library.questionTwo()
+library.questionThree()
+
+# library.createDictionaryBigram()
