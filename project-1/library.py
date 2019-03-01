d88e09befb4465f231423921c005058a2094a589
Daniel Constantine <danielkevinconstantine@gmail.com>
2019-03-01 18:40:51 -0500

    Adding updates

--- project-1/library.py
+++ project-1/library.py
@@ -1,5 +1,6 @@
-#import this so when I divide number it would return float, for python 2.7
+# import this so when I divide number it would return float, for python 2.7
 from __future__ import division
+
 
 def writeToFile(url, container):
     bt = open(url, 'w')
@@ -8,10 +9,13 @@ def writeToFile(url, container):
     bt.close()
 
 #-- padding sentences --#
+
+
 def padSentence():
     paddingSentence('brown-train')  # padding the brown-train file
     paddingSentence('learner-test')  # padding the learner-test file
     paddingSentence('brown-test')  # padding the brown-test file
+
 
 def paddingSentence(url):
     # constant to append and prepend
@@ -36,6 +40,8 @@ def paddingSentence(url):
     writeToFile(url+'-after.txt', container)
 
 #-- create dictionary --#
+
+
 def createDictionaryUnigram():
     d = dict()  # my dictionary
 
@@ -51,6 +57,7 @@ def createDictionaryUnigram():
 
     writeDictToFile('dictionary-unigram-before-unk-dataset', d)
     return d
+
 
 def createDictionaryBigram():
     d = dict()  # my dictionary
@@ -71,12 +78,15 @@ def createDictionaryBigram():
     writeDictToFile('dictionary-bigram-before-unk-dataset', d)
     return d
 
+
 def writeDictToFile(url, d):
     f = open(url + '.txt', 'w')
     f.write(str(d))
     f.close
 
 # replacing the word with <unk>
+
+
 def replaceOccuring():
     d = createDictionaryUnigram()  # my dictionary
     print('--Replacing the word occured once in brown-train file with <unk>--')
@@ -87,6 +97,7 @@ def replaceOccuring():
 
     print('--Replacing the word not appeared in brown-test file with <unk>--')
     replaceNotOccuring(d, 'learner-test-after')
+
 
 def replaceOccuringOnce(d, url):
     # replace all data occurred in the training data once with <unk>
@@ -100,7 +111,7 @@ def replaceOccuringOnce(d, url):
         for oneString in currentLine.split(' '):
             if oneString == '<s>':
                 txt = oneString
-            elif d.get(oneString) == 1: # replace the word that appear only once with the word <unk>
+            elif d.get(oneString) == 1:  # replace the word that appear only once with the word <unk>
                 txt = txt + ' <unk>'
             else:
                 txt = txt + ' ' + oneString
@@ -108,6 +119,7 @@ def replaceOccuringOnce(d, url):
     openFile.close()
 
     writeToFile(url+'-replaced-unk.txt', container)
+
 
 def replaceNotOccuring(d, url):
     # replace all data not occurred in the training data with <unk>
@@ -121,7 +133,7 @@ def replaceNotOccuring(d, url):
         for key in currentLine.split(' '):
             if key == '<s>':
                 txt = key
-            elif key not in d.keys(): # replace the word that not appear in the training with <unk>
+            elif key not in d.keys():  # replace the word that not appear in the training with <unk>
                 txt = txt + ' <unk>'
             else:
                 txt = txt + ' ' + key
@@ -130,9 +142,11 @@ def replaceNotOccuring(d, url):
 
     writeToFile(url + '-replaced-unk.txt', container)
 
+
 def questionOne():
     mySet = createSet('brown-train-after-replaced-unk')  # my set
     print('Question 1: ', len(mySet))
+
 
 def questionTwo():
     count = 0
@@ -145,64 +159,77 @@ def questionTwo():
 
     print('Question 2: ', count)
 
+
 def questionThree():
     myDict = createDictionaryUnigram()
     sizeTraining = countSize('brown-train-after')
     print('Size of training set ', sizeTraining)
 
     # ** BROWN-TEST ** #
-    brownSet = createSet('brown-test-after') 
-    countTokenBrownNotAppearsTrainning = countTokens(myDict, 'brown-test-after')
+    brownSet = createSet('brown-test-after')
+    countTokenBrownNotAppearsTrainning = countTokens(
+        myDict, 'brown-test-after')
     countTypesBrownNotAppearTraining = countTypes(myDict, brownSet)
     sizeBrownTest = countSize('brown-test-after')
 
-    print(countTokenBrownNotAppearsTrainning, 'token')
-    print(countTypesBrownNotAppearTraining, 'type')
-    print(sizeBrownTest, 'size')
-
+    print(countTokenBrownNotAppearsTrainning,
+          'how many tokens in brown not appear in training')
+    print(countTypesBrownNotAppearTraining,
+          'how many types in brown not appear in training')
+    print(sizeBrownTest, 'the size of the brown test / all words in brown test')
 
     # COUNT OF HOW MANY WORD NOT APPEAR / COUNT OF HOW MANY WORD IN TRAINING
     # COUNT OF HOW MANY THOSE ^ / SIZE OF THE TEST
     percentageTokenBrown = countTokenBrownNotAppearsTrainning/sizeTraining
 
     print(percentageTokenBrown, 'percentage token brown')
-    percentageTokenBrown =  (percentageTokenBrown / sizeBrownTest) * 100
+    percentageTokenBrown = (percentageTokenBrown / sizeBrownTest) * 100
 
-    percentageTypesBrown =  (countTypesBrownNotAppearTraining/sizeTraining)
-    percentageTypesBrown =  (percentageTypesBrown / sizeBrownTest) * 100
+    percentageTypesBrown = (countTypesBrownNotAppearTraining/sizeTraining)
+    percentageTypesBrown = (percentageTypesBrown / sizeBrownTest) * 100
 
     # ** LEARNER-TEST ** #
     learnerSet = createSet('learner-test-after')
-    countTokenLearnerNotAppearsTraining = countTokens(myDict, 'learner-test-after')
-    countTypesLearnerNotAppearTraining = countTypes(myDict, learnerSet) 
+    countTokenLearnerNotAppearsTraining = countTokens(
+        myDict, 'learner-test-after')
+    countTypesLearnerNotAppearTraining = countTypes(myDict, learnerSet)
     sizeLearnerTest = countSize('learner-test-after')
 
-    percentageTokenLearner =  (countTokenLearnerNotAppearsTraining/sizeTraining)
-    percentageTokenLearner =  (percentageTokenLearner / sizeLearnerTest) * 100
+    percentageTokenLearner = (countTokenLearnerNotAppearsTraining/sizeTraining)
+    percentageTokenLearner = (percentageTokenLearner / sizeLearnerTest) * 100
 
-    percentageTypesLearner =  (countTypesLearnerNotAppearTraining/sizeTraining)
-    percentageTypesLearner =  (percentageTypesLearner / sizeLearnerTest) * 100
+    percentageTypesLearner = (countTypesLearnerNotAppearTraining/sizeTraining)
+    percentageTypesLearner = (percentageTypesLearner / sizeLearnerTest) * 100
 
-    #PRINTING OUT
-
-    print(countTokenBrownNotAppearsTrainning, ' count token appears brown test')
-    print(countTypesBrownNotAppearTraining, ' count types appears brown test')
-    print(sizeBrownTest, ' size of brown test')
-
-    print('Percentage of tokens in Brown not appear in training ', percentageTokenBrown)
-    print('Percentage of types in Brown not appear in training ', percentageTypesBrown)
-
-    print(countTypesLearnerNotAppearTraining, ' count type appears learner test')
-    print(countTokenLearnerNotAppearsTraining, ' count token appears learner test')
+    print(countTypesLearnerNotAppearTraining,
+          ' count types appears learner test')
+    print(countTokenLearnerNotAppearsTraining,
+          ' count token appears learner test')
     print(sizeLearnerTest, ' size of learner test')
 
+    print('Percentage of tokens in Learner not appear in training ',
+          percentageTokenLearner)
+    print('Percentage of types in Learner not appear in training ',
+          percentageTypesLearner)
 
-    print('Percentage of tokens in Learner not appear in training ', percentageTokenLearner)
-    print('Percentage of types in Learner not appear in training ', percentageTypesLearner)
+    # PRINTING OUT
+
+    # print(countTokenBrownNotAppearsTrainning,
+    #       ' count token appears brown test')
+    # print(countTypesBrownNotAppearTraining, ' count types appears brown test')
+    # print(sizeBrownTest, ' size of brown test')
+
+    # print('Percentage of tokens in Brown not appear in training ',
+    #       percentageTokenBrown)
+    # print('Percentage of types in Brown not appear in training ',
+    #       percentageTypesBrown)
+
 
 # helper function for question 3 #
+
+
 def createSet(url):
-    thisSet = set() 
+    thisSet = set()
 
     openFile = open(url + '.txt', 'r')
     openRead = openFile.readlines()
@@ -214,15 +241,19 @@ def createSet(url):
 
     return thisSet
 
-def countTypes(myDict, mySet): #return how many unique value in set, not appear in the map/dictionary
+
+# return how many unique value in set, not appear in the map/dictionary
+def countTypes(myDict, mySet):
     count = 0
     for item in mySet:
         if item not in myDict:
             count = count + 1
-    
+
     return count
 
-def countTokens(myDict, url): #return how many tokens (all words) in the file not appear in the dict
+
+# return how many tokens (all words) in the file not appear in the dict
+def countTokens(myDict, url):
     count = 0
     openFile = open(url + '.txt', 'r')
     openRead = openFile.readlines()
@@ -234,7 +265,8 @@ def countTokens(myDict, url): #return how many tokens (all words) in the file no
     openFile.close()
     return count
 
-def countSize(url): #return how many words in a text file
+
+def countSize(url):  # return how many words in a text file
     count = 0
     openFile = open(url + '.txt', 'r')
     openRead = openFile.readlines()
@@ -244,6 +276,7 @@ def countSize(url): #return how many words in a text file
     openFile.close()
     return count
 # end of helper function for question 3 #
+
 
 def questionFour():
     myDict = createDictionaryBigram()
@@ -262,6 +295,8 @@ def questionFour():
     countBigramInLearner = countSizeBigram(learnerUrl)
 
 # helper function for question 4 #
+
+
 def createSetBigram(url):
     thisSet = set()
     openFile = open(url + '.txt', 'r')
@@ -273,8 +308,9 @@ def createSetBigram(url):
             combinedString = split[index] + ',' + split[index+1]
             if combinedString not in thisSet:
                 thisSet.add(combinedString)
-    
+
     return thisSet
+
 
 def countTypesBigram(myDict, mySet):
 
@@ -285,11 +321,12 @@ def countTypesBigram(myDict, mySet):
 
     return count
 
+
 def countTokensBigram(myDict, url):
     count = 0
     openFile = open(url + '.txt', 'r')
     openRead = openFile.readlines()
-    
+
     for currentLine in openRead:
         split = currentLine.split(' ')
         for index in range(len(split) - 1):
@@ -298,6 +335,7 @@ def countTokensBigram(myDict, url):
             if combinedString not in myDict:
                 count = count + 1
     return count
+
 
 def countSizeBigram(url):
     count = 0
@@ -310,5 +348,4 @@ def countSizeBigram(url):
             count = count + 1
 
     return count
-# end of helper function for question 4 # 
-
+# end of helper function for question 4 #
